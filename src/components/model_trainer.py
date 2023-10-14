8a89d59349886bfda05320c2a26fd6319cab32f5
Hello <kir26tan07@gmail.com>
2023-10-14 12:16:22 +0530

    Hyperparameter

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -10,7 +10,6 @@ from sklearn.ensemble import (
 
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
-from sklearn.neighbors import KNeighborsRegressor
 from sklearn.tree import DecisionTreeRegressor
 from xgboost import XGBRegressor
 
@@ -42,14 +41,46 @@ class ModelTrainer:
                 'Decision Tree': DecisionTreeRegressor(),
                 'Gradient Boosting': GradientBoostingRegressor(),
                 'Linear Regression': LinearRegression(),
-                'K-Neighbors': KNeighborsRegressor(),
                 'XGB': XGBRegressor(),
                 'AdaBoosting': AdaBoostRegressor()
             }
             
-            model_report : dict=evaluate_model(X_train=X_train, y_train=y_train, 
+            params = {
+                'Decision Tree':{
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "Random Forest":{
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+                    # 'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Gradient Boosting":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "XGB":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "AdaBoosting":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+                
+            }
+            
+            model_report : dict=evaluate_model(X_train = X_train, y_train = y_train, 
                                                X_test = X_test, y_test = y_test,
-                                               models=models)
+                                               models = models, param = params)
             
             best_model_score = max(sorted(model_report.values()))
             
